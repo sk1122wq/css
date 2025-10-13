ccc4ce4898825c6aa8306d2ed1019b329c33e341
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-10-13 11:28:56 +0500

    Add files via upload

---
+++ sum.1.py
@@ -0,0 +1,36 @@
+# def cal_sum(a,b):
+#     sum=a+b
+#     print(sum)
+#     return sum
+# cal_sum(2,3)
+# cal_sum(5,6)
+# cal_sum(10,5)
+# def find_Ave(a,b,c):
+#     Average=a+b+c/2
+#     print(Average)
+#     return Average
+# find_Ave(5,2,6)
+# Types of function in python 1) Built-in function(print(),len(),type(),ranget() and User define function() in print fuction space is also built in)
+# user define function cal()
+# default parameters( Assigng a default to parameter which is used when no argument is passed.
+# # def cal_prod(a, b=4): # default gives always at the last
+#     print(a+b)
+#     return a+b
+# cal_prod(1)
+# practise question 1) WAp to print the length of a list (list is the parameter)
+# cities=["lahore","karachi","islamabad"]
+# def print_len(list):
+#     print(len(list))
+#     print_len(cities)
+# WAP to find the factorial of n.(n is the parameter)
+# def cal_fact(n):
+#     fact=1
+#     for i in range(1,n+1):
+#         fact*=i
+#         print(fact)
+#         cal_fact(6)
+
+
+    
+    
+

1731a5ebed3c171f1db7f3eb7c507b872a5fa210
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-11-07 13:14:31 +0500

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,12 @@
+# def cal_sum(a,b):
+#     sum=a+b
+#     print(sum)
+#     return sum
+# cal_sum(2,3)
+# cal_sum(5,6)
+# cal_sum(10,5)
+def find_Ave(a,b,c):
+    Average=a+b+c/2
+    print(Average)
+    return Average
+find_Ave(5,2,6)
